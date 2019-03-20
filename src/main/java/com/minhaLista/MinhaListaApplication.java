2321373be6a5b6643681d5c7aa752c6f73980d6f
Valdecir Padovani Junior <valdecir.junior@lago.local>
2019-03-20 12:53:24 -0300

    Inicio do projeto

---
+++ src/main/java/com/minhaLista/MinhaListaApplication.java
@@ -0,0 +1,13 @@
+package com.minhaLista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaListaApplication.class, args);
+	}
+
+}
